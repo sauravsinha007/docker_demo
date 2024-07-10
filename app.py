c5fa53efcb25b79a0014c8eb174da7530e1bec82
saurav sinha <sauravsinha007@gmail.com>
2024-07-10 09:59:11 +0530

    docker run sucessfullly

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-  return 'Hello World Docker!'
+    return {"message":"Hello world (Python app) with Docker"}
 
-if __name__ == '__main__':
-  app.run()
+app.run()
+
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#   return 'Hello World Docker!'
+
+# if __name__ == '__main__':
+#   #app.run()
+#   app.run(host="0.0.0.0", port=5000, debug=True)
 
 
